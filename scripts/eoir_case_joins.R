b73f9f96a9132d0c0d5b4d49c057c5a5ac1ea0ef
Graeme Blair <graemeblair@users.noreply.github.com>
2025-08-03 19:44:18 -0700

    split excel sheets

--- scripts/eoir_case_joins.R
+++ scripts/eoir_case_joins.R
@@ -105,7 +105,19 @@ arrow::write_feather(
   "outputs/cases.feather"
 )
 
+
+
 arrow::write_feather(cases, "outputs/cases.feather")
-writexl::write_xlsx(cases, "outputs/cases.xlsx")
 haven::write_dta(cases, "outputs/cases.dta")
 haven::write_sav(cases, "outputs/cases.sav")
+
+# split into sheets for Excel (max 1 million rows per sheet)
+sheet_size <- 1e6
+n_sheets <- ceiling(nrow(cases) / sheet_size)
+
+cases |>
+  mutate(.sheet_id = rep(seq_len(n_sheets), each = sheet_size, length.out = n())) |>
+    #  gl(n_sheets, sheet_size, n(), labels = FALSE)) |>
+  group_split(.sheet_id, .keep = FALSE) |>
+  set_names(sprintf("%s_%02d", "Sheet", seq_len(n_sheets))) |>
+  writexl::write_xlsx("outputs/cases.xlsx")
